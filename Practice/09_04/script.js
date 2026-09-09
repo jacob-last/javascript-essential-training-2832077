2f67ec842c35447c4e6f42d2c394998aee575a42
Jacob Last <jacob.last@anaplan.com>
2026-09-09 16:59:48 +0100

    End of section 9

--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -5,3 +5,29 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+
+let squares = document.querySelectorAll(".cell");
+
+squares.forEach((square) => {
+  square.setAttribute("colourid", 0);
+  square.classList.add("colour0");
+  square.addEventListener("click", (event) => {
+    let colourID = square.getAttribute("colourid");
+    square.classList.remove("colour" + colourID);
+
+    if (colourID == 7) {
+      colourID = -1;
+    }
+
+    square.setAttribute("colourID", Number(colourID) + 1);
+    square.classList.add("colour" + (Number(colourID) + 1));
+  });
+
+  square.addEventListener("mouseenter", (event) => {
+    square.classList.add("border");
+  });
+
+  square.addEventListener("mouseleave", (event) => {
+    square.classList.remove("border");
+  });
+});
